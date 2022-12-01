30cb7843d2ee0458548578be8bd2397cf81c5b20
Evan Rittenhouse <erittenhouse@fearless.tech>
2022-12-01 13:51:06 -0500

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,51 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxSum = 0
+        var currentSum = 0
+        for (calorieCount in input) {
+            if (calorieCount.isBlank()) {
+                if (currentSum > maxSum) maxSum = currentSum
+                currentSum = 0
+                continue
+            }
+
+            currentSum += calorieCount.toInt()
+        }
+
+        if (currentSum != 0 && maxSum < currentSum) maxSum = currentSum
+
+        return maxSum
+    }
+
+    fun addSumToQueue(listToModify: MutableList<Int>, newValue: Int) {
+        listToModify += newValue
+        listToModify.sort()
+        if (listToModify.size > 3) listToModify.removeFirst()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val topSums = mutableListOf<Int>()
+        var currentSum = 0
+        for (calorieCount in input) {
+            if (calorieCount.isBlank()) {
+                addSumToQueue(topSums, currentSum)
+                currentSum = 0
+                continue
+            }
+
+            currentSum += calorieCount.toInt()
+        }
+
+        if (currentSum != 0) addSumToQueue(topSums, currentSum)
+
+        println("Part 2 top sums: $topSums")
+        return topSums.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,12 +5,13 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("src/inputs", "$name.txt")
     .readLines()
 
 /**
  * Converts string to md5 hash.
  */
+@Suppress("unused")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
